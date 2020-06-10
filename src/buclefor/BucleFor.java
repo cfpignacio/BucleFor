ef6caa4f15362a6d7093f75cb851549067a8d84c
Ignaciom <ignacio.camillucci@gmail.com>
2020-06-10 19:34:45 -0300

    Commit
    add -> forLimite

--- src/buclefor/BucleFor.java
+++ src/buclefor/BucleFor.java
@@ -5,6 +5,8 @@
  */
 package buclefor;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Notebook
@@ -16,6 +18,33 @@ public class BucleFor {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+// bucle for
+//       for( int i = 1; i< 100; i++){
+//           
+//           System.out.println(i);
+//       
+//       }
+
+    int inicio,fin;
+    Scanner leer;
+    
+    // guardamos el valor de limite
+    leer = new Scanner(System.in);
+    
+    System.out.println("Inicio: ");
+    inicio = leer.nextInt();
+    
+    System.out.println("Fin: ");
+    fin = leer.nextInt();
+    
+    
+    for( int i = inicio; i <= fin; i++){
+        System.out.println("El valor de i es -> " + i);
+        
+    }
+    
+
+
     }
     
 }
